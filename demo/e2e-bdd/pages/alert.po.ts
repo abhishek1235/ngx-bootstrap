1cd043bb0356b395813ef9ecae82f324841ee268
Yevheniia Mazur <yevheniia.mazur@valor-software.com>
2017-11-27 19:49:32 +0200

    feat(tests): covering navigation feature with bdd

---
+++ demo/e2e-bdd/pages/alert.po.ts
@@ -0,0 +1,8 @@
+import { BasePo } from '../shared/pages/basePage.po';
+import { $, browser, ElementFinder } from 'protractor';
+
+export class AlertPo extends BasePo {
+  templateUrl = 'alerts';
+  pageUrl = (browser.baseUrl as string) + this.templateUrl;
+  contentTitle: ElementFinder = $('.content h1');
+}
